63ff9e9a6d235cbc51612900238e9ce32de0b04e
Toshimitsu-M <arrinfluedshwman3+git@gmail.com>
2024-08-11 20:37:22 +0900

    Build project and update distribution files

--- src/App.vue
+++ src/App.vue
@@ -42,7 +42,7 @@
         <v-list-item>
           <v-list-item-content>
             <v-list-item-title class="text-h6">
-              Navigation Lists
+              サイドバー
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
