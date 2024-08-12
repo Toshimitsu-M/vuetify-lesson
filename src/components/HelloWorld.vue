b6e7061ee1c8f42dd66bd394383e0d78d769fa95
Toshimitsu-M <arrinfluedshwman3+git@gmail.com>
2024-08-12 17:04:26 +0900

    ok

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          <div>Welcome to the Vuetify 3 Alpha</div>
+          Welcome to the Vuetify 3 Alpha
         </h1>
 
         <p class="subheading font-weight-regular">
